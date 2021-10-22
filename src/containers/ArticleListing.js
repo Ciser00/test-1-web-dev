fda5f101e74787da6a4ca6b5f0b023ee4009c894
Ciser00 <47250680+Ciser00@users.noreply.github.com>
2021-10-22 08:07:57 -0400

    Last Update

--- src/containers/ArticleListing.js
+++ src/containers/ArticleListing.js
@@ -1,19 +1,17 @@
-import React from 'react';
+import React from "react";
 import ArticleCard from "../components/ArticleCard";
-import Data from '../components/data.json'
+import Data from "../components/data.json";
 
 function ArticleListings(){
 //console.log(Data);
-  const articleTitle = 'Articles'
+  const articleTitle=`Articles`;
   return(
-    <div className = "PageWrapper">
+    <div className="PageWrapper">
       <h2 className="HeaderOneStyle">{articleTitle}</h2>
       {Data.map((article, key) => (
-        <ArticleCard article={article} key = {key} />
+        <ArticleCard article={article} key={key} />
       ))}
-      <ArticleCard/>
     </div>
-
   );
 }
 
